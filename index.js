6990c50ef10b9ed67a648547ac761119dfd90f68
jomanaaljarallah <jarallahjomana@gmal.com>
2023-04-22 08:53:25 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(x ,y) {
+    return x+y ;
+    
+}
+add(2,4)
+function subtract(x,y) {
+    return x-y ;
+    
+}
+function multiply(x,y){
+     return x*y ;
+}
+ function divide(x,y){
+    return x/y ;
+ }
+ function increment(n) {
+    return n+=1;
+
+ }
+ function decrement(n){
+    return n-=1;
+
+ }
+ function makeInt(n){
+  return  parseInt(n, 10);
+    //parseInt("2.55");
+ }
+ makeInt(15);
+ function preserveDecimal(n){
+    return parseFloat(n);
+ }
+ preserveDecimal(5.77);
